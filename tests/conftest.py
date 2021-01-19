10db1b262f8705da0a17e250a2d91c7d0bf51147
dangchinh25 <dangchinh2501@gmail.com>
2021-01-19 14:12:50 +0700

    Remove unnecessary import

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,8 @@
 import pytest
 import os
 
-from main.controllers.category import app, get_category, get_categories, get_category_items, create_item, create_category
-from main.controllers.item import app, get_item, update_item, delete_item
+from main.controllers.category import get_category, get_categories, get_category_items, create_item, create_category
+from main.controllers.item import get_item, update_item, delete_item
 from main.controllers.user import app, signin, signup, get_current_user
 from main.db import db
 from tests.setup_db import drop_table, generate_categories, generate_users, generate_items
