de98215063adef6f80b4f77a87f06e40c5a1fcab
dangchinh25 <dangchinh2501@gmail.com>
2021-01-24 23:34:26 +0700

    Make the file create_tables.py work

--- create_tables.py
+++ create_tables.py
@@ -1,5 +1,9 @@
-from main.db import db
 from main.app import app
+from main.db import db
+from main.models.user import UserModel
+from main.models.item import ItemModel
+from main.models.category import CategoryModel
+
 
 with app.app_context():
     db.create_all()
